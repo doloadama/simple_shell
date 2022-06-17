a20cd3d3165bfed84156beb9857089db72bd315c
Dolo Adama <doloadama@outlook.com>
2022-06-17 12:02:15 +0000

    UPDATES

--- fun.c
+++ fun.c
@@ -1,112 +1,121 @@
 #include "shell.h"
 
 /**
- * _strcat - ncatenates two strings
- *
- * @dest: Destination string
- *
- * @src: Origin string
- * Return: Always 0
- */
-
-
-
-char *_strcat(char *dest, char *src)
+* _puts - writes a string to standard output
+* @str: string to write
+*
+* Return: number of chars printed or -1 on failure
+*/
+ssize_t _puts(char *str)
 {
-int i, j = 0;
+ssize_t num, len;
 
-for (i = 0; dest[i] != '\0'; i++)
+num = _strlen(str);
+len = write(STDOUT_FILENO, str, num);
+if (len != num)
 {
+perror("Fatal Error");
+return (-1);
 }
-for (j = 0; src[j] != '\0'; j++)
-{
-dest[i] = src[j];
-i++;
-
-}
-dest[i] = '\0';
-return (dest);
-}
-
-
-
-/**
- * _strcmp - Compares two strings
- *
- * @s1: a string
- * @s2: Another string
- *
- * Return: multiple returns
- *
- */
-
-int _strcmp(char *s1, char *s2)
-{
-int i = 0;
-int val;
-
-while (s1[i] != 0 && s2[i] != 0 && s1[i] == s2[i])
-i++;
-val = s1[i] - s2[i];
-return (val);
-
+return (len);
 }
 
 /**
- * _strlen - gets the size of a string
- * @s: the string to measure
- * Return: the length of the string
- */
+* _strdup - returns pointer to new mem alloc space which contains copy
+* @strtodup: string to be duplicated
+* Return: a pointer to the new duplicated string
+*/
+char *_strdup(char *strtodup)
+{
+char *copy;
 
-int _strlen(char *s)
+int len, i;
+
+if (strtodup == 0)
+return (NULL);
+
+for (len = 0; strtodup[len]; len++)
+;
+copy = malloc((len + 1) * sizeof(char));
+
+for (i = 0; i <= len; i++)
+copy[i] = strtodup[i];
+
+return (copy);
+}
+
+/**
+* _strcmpr - compares two strings
+* @strcmp1: first string, of two, to be compared in length
+* @strcmp2: second string, of two, to be compared
+* Return: 0 on success, anything else is a failure
+*/
+int _strcmpr(char *strcmp1, char *strcmp2)
 {
 int i;
 
-for (i = 0; s[i] != 0; i++)
-;
-return (i);
-}
-
-/**
- *_strncmp -  function that compares two strings.
- *@s1: string one
- *@s2: string two
- *@n: number of characters
- * Return: diference
- */
-
-size_t _strncmp(char *s1, char *s2, size_t n)
+i = 0;
+while (strcmp1[i] == strcmp2[i])
 {
-size_t i, j;
-
-for (j = 0; s1[j] != '\0' && j < n; j++)
-{
-i = s1[j] - s2[j];
-
-if (i != 0)
-{
-return (i);
-}
-}
+if (strcmp1[i] == '\0')
 return (0);
+i++;
+}
+return (strcmp1[i] - strcmp2[i]);
 }
 
 /**
- * _strcpy - copies the string pointed to by src into dest
- * @dest: destination of the copy
- * @src: source of the copy
- *
- * Return: char pointer to dest
- */
-
-char *_strcpy(char *dest, char *src)
+* _strcat - concatenates two strings
+* @strc1: first string
+* @strc2: second string
+* Return: pointer
+*/
+char *_strcat(char *strc1, char *strc2)
 {
-int i = 0;
+char *newstring;
+unsigned int len1, len2, newlen, i, j;
 
-for (i = 0; src[i] != '\0'; i++)
+len1 = 0;
+len2 = 0;
+if (strc1 == NULL)
+len1 = 0;
+else
 {
-dest[i] = src[i];
+for (len1 = 0; strc1[len1]; len1++)
+;
 }
-dest[i + 1] = 0;
-return (dest);
+if (strc2 == NULL)
+len2 = 0;
+else
+{
+for (len2 = 0; strc2[len2]; len2++)
+;
+}
+newlen = len1 + len2 + 2;
+newstring = malloc(newlen * sizeof(char));
+if (newstring == NULL)
+return (NULL);
+for (i = 0; i < len1; i++)
+newstring[i] = strc1[i];
+newstring[i] = '/';
+for (j = 0; j < len2; j++)
+newstring[i + 1 + j] = strc2[j];
+newstring[len1 + len2 + 1] = '\0';
+return (newstring);
+}
+
+/**
+* _strlen - returns the length of a string
+* @str: string to be measured
+* Return: length of string
+*/
+unsigned int _strlen(char *str)
+{
+unsigned int len;
+
+len = 0;
+
+for (len = 0; str[len]; len++)
+;
+return (len);
 }
--- shell.h
+++ shell.h
@@ -1,36 +1,180 @@
-#ifndef SHELL_H
-#define SHELL_H
+#include "shell.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include <errno.h>
+/**
+* path_execute - executes a command in the path
+* @command: full path to the command
+* @vars: pointer to struct of variables
+*
+* Return: 0 on succcess, 1 on failure
+*/
+int path_execute(char *command, vars_t *vars)
+{
+pid_t child_pid;
 
+if (access(command, X_OK) == 0)
+{
+child_pid = fork();
+if (child_pid == -1)
+print_error(vars, NULL);
+if (child_pid == 0)
+{
+if (execve(command, vars->av, vars->env) == -1)
+print_error(vars, NULL);
+}
+else
+{
+wait(&vars->status);
+if (WIFEXITED(vars->status))
+vars->status = WEXITSTATUS(vars->status);
+else if (WIFSIGNALED(vars->status) && WTERMSIG(vars->status) == SIGINT)
+vars->status = 130;
+return (0);
+}
+vars->status = 127;
+return (1);
+}
+else
+{
+print_error(vars, ": Permission denied\n");
+vars->status = 126;
+}
+return (0);
+}
 
-int lsh_ctrld(char **args);
-int lsh_cd(char **args);
-int lsh_help(char **args);
-extern char **environ;
-int lsh_exit(char **args);
-int _strcmp(char *s1, char *s2);
-size_t _strncmp(char *s1, char *s2, size_t n);
-int _strlen(char *s);
-char *_strcpy(char *dest, char *src);
-char *_strcat(char *dest, char *src);
-int _putchar(char c);
+/**
+* find_path - finds the PATH variable
+* @env: array of environment variables
+*
+* Return: pointer to the node that contains the PATH, or NULL on failure
+*/
+char *find_path(char **env)
+{
+char *path = "PATH=";
+unsigned int i, j;
 
-char *_get_path(char **env);
-int _values_path(char **arg, char **env);
-char *_getline_command(void);
-void _getenv(char **env);
-char **tokenize(char *lineptr);
-void _exit_command(char **args, char *lineptr, int _exit);
-int _fork_fun(char **arg, char **av, char **env,
-char *lineptr, int np, int c);
+for (i = 0; env[i] != NULL; i++)
+{
+for (j = 0; j < 5; j++)
+if (path[j] != env[i][j])
+break;
+if (j == 5)
+break;
+}
+return (env[i]);
 
+}
 
-#endif /* SHELL_H */
+/**
+* check_for_path - checks if the command is in the PATH
+* @vars: variables
+*
+* Return: void
+*/
+void check_for_path(vars_t *vars)
+{
+char *path, *path_dup = NULL, *check = NULL;
+unsigned int i = 0, r = 0;
+char **path_tokens;
+struct stat buf;
+
+if (check_for_dir(vars->av[0]))
+r = execute_cwd(vars);
+else
+{
+path = find_path(vars->env);
+if (path != NULL)
+{
+path_dup = _strdup(path + 5);
+path_tokens = tokenize(path_dup, ":");
+for (i = 0; path_tokens && path_tokens[i]; i++, free(check))
+{
+check = _strcat(path_tokens[i], vars->av[0]);
+if (stat(check, &buf) == 0)
+{
+r = path_execute(check, vars);
+free(check);
+break;
+}
+}
+free(path_dup);
+if (path_tokens == NULL)
+{
+vars->status = 127;
+new_exit(vars);
+}
+}
+if (path == NULL || path_tokens[i] == NULL)
+{
+print_error(vars, ": not found\n");
+vars->status = 127;
+}
+free(path_tokens);
+}
+if (r == 1)
+new_exit(vars);
+}
+
+/**
+* execute_cwd - executes the command in the current working directory
+* @vars: pointer to struct of variables
+*
+* Return: 0 on success, 1 on failure
+*/
+int execute_cwd(vars_t *vars)
+{
+pid_t child_pid;
+struct stat buf;
+
+if (stat(vars->av[0], &buf) == 0)
+{
+if (access(vars->av[0], X_OK) == 0)
+{
+child_pid = fork();
+if (child_pid == -1)
+print_error(vars, NULL);
+if (child_pid == 0)
+{
+if (execve(vars->av[0], vars->av, vars->env) == -1)
+print_error(vars, NULL);
+}
+else
+{
+wait(&vars->status);
+if (WIFEXITED(vars->status))
+vars->status = WEXITSTATUS(vars->status);
+else if (WIFSIGNALED(vars->status) && WTERMSIG(vars->status) == SIGINT)
+vars->status = 130;
+return (0);
+}
+vars->status = 127;
+return (1);
+}
+else
+{
+print_error(vars, ": Permission denied\n");
+vars->status = 126;
+}
+return (0);
+}
+print_error(vars, ": not found\n");
+vars->status = 127;
+return (0);
+}
+
+/**
+* check_for_dir - checks if the command is a part of a path
+* @str: command
+*
+* Return: 1 on success, 0 on failure
+*/
+int check_for_dir(char *str)
+{
+unsigned int i;
+
+for (i = 0; str[i]; i++)
+{
+if (str[i] == '/')
+return (1);
+}
+return (0);
+}
